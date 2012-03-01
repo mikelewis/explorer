a1c125c7688693f9b22af5ee9f77f1b6c674d6fc
Mike Lewis <ft.mikelewis@gmail.com>
2012-02-29 21:57:04 -0800

    Setup configuration files

--- project/Build.scala
+++ project/Build.scala
@@ -3,15 +3,21 @@ import Keys._
 
 object ExplorerBuild extends Build {
   lazy val root = Project(id = "explorer",
-    base = file(".")) aggregate(common, fetcher, processor)
+    base = file(".")
+  ) aggregate(common, fetcher, processor)
 
-  lazy val fetcher = Project(id = "fetcher",
-    base = file("fetcher")) dependsOn(common)
+lazy val fetcher = Project(id = "fetcher",
+  base = file("fetcher"),
+  settings = Defaults.defaultSettings ++ Seq((unmanagedClasspath in Compile) += Attributed.blank(file("fetcher/src/main/resources")))
+) dependsOn(common)
 
   lazy val processor = Project(id = "processor",
-    base = file("processor")) dependsOn(common)
+    base = file("processor"),
+    settings = Defaults.defaultSettings ++ Seq((unmanagedClasspath in Compile) += Attributed.blank(file("processor/src/main/resources")))
+  ) dependsOn(common)
 
   lazy val common = Project(id = "common",
-    base = file("common"))
+    base = file("common"),
+    settings = Defaults.defaultSettings ++ Seq((unmanagedClasspath in Compile) += Attributed.blank(file("common/src/main/resources")))
+  )
 }
-
